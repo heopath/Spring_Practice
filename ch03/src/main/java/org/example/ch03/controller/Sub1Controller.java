cbb01f750acb6ca3a24b6c5d3fb98047b76944b7
heopath <m8765000@naver.com>
2026-06-16 09:54:33 +0900

    스프링 부트 실습

---
+++ ch03/src/main/java/org/example/ch03/controller/Sub1Controller.java
@@ -0,0 +1,26 @@
+package org.example.ch03.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+// 사용자 요청을 처리하는 컨트롤러 생성
+@Controller
+public class Sub1Controller {
+
+    @GetMapping("/sub1/hello")
+    public String hello(){
+
+        // 반환되는 문자열 뒤에 확장자 .html 붙음, 해당 문자열로 foward
+        return "sub1/hello";
+    }
+
+    @GetMapping("/sub1/welcome")
+    public String welcome(){
+        return "sub1/welcome";
+    }
+
+    @GetMapping("/sub1/greeting")
+    public String greeting(){
+        return "sub1/greeting";
+    }
+}
