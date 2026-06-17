fa67b3d4cc5dd904b2425df6d72d508f70e80a73
heopath <m8765000@naver.com>
2026-06-17 11:01:08 +0900

    Mapper로 DB 연결 및 서비스 구현

--- ch05/src/main/java/org/example/ch05/controller/User1Controller.java
+++ ch05/src/main/java/org/example/ch05/controller/User1Controller.java
@@ -1,11 +1,21 @@
 package org.example.ch05.controller;
 
 
+import lombok.RequiredArgsConstructor;
+import org.example.ch05.dto.User1DTO;
+import org.example.ch05.service.User1Service;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
+
+@RequiredArgsConstructor
 @Controller
 public class User1Controller {
+
+    // @RequiredArgsConstructor 어노테이션으로 의존객체 주입 ⭐⭐⭐⭐⭐
+    private final User1Service service;
 
     @GetMapping("/user1/list")
     public String list(){
@@ -16,6 +26,19 @@ public class User1Controller {
     public String register(){
         return "/user1/register";
     }
+
+    @PostMapping("/user1/register")
+    public String register(User1DTO dto){
+        System.out.println(dto);
+        
+        // 등록 서비스 호출
+        service.register(dto);
+
+
+        return "redirect:/user1/list";
+    }
+
+
 
     @GetMapping("/user1/modify")
     public String modify(){
