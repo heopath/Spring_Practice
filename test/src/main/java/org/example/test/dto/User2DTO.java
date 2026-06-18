58f990322f60db777ea0e83f6dd1fa1b99611c1c
heopath <m8765000@naver.com>
2026-06-18 09:49:37 +0900

    로깅과 테스트 실습

---
+++ test/src/main/java/org/example/test/dto/User2DTO.java
@@ -0,0 +1,17 @@
+package org.example.test.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User2DTO {
+    private String userid;
+    private String name;
+    private String birth;
+    private String addr;
+
+}
