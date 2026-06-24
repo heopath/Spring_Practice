b84932919268ed3c488071d40fafec2fa278ea90
heopath <m8765000@naver.com>
2026-06-24 09:55:33 +0900

    gradle security 추가, authentication 로그 확인 실습

--- ch07/src/main/java/org/example/ch07/controller/UserController.java
+++ ch07/src/main/java/org/example/ch07/controller/UserController.java
@@ -1,12 +1,19 @@
 package org.example.ch07.controller;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.example.ch07.dto.UserDTO;
+import org.example.ch07.entity.User;
+import org.example.ch07.security.MyUserDetails;
 import org.example.ch07.service.UserService;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+@Log4j2
 @RequiredArgsConstructor
 @Controller
 public class UserController {
@@ -31,5 +38,18 @@ public class UserController {
 
         return "redirect:/user/login?register=success";
     }
+    @GetMapping("/user/info")
+    public String info(Model model){
 
+        // 시큐리티 사용자 인증객체
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        log.info(authentication);
+
+        MyUserDetails details = (MyUserDetails) authentication.getPrincipal();
+        User user = details.getUser();
+
+        model.addAttribute(user);
+
+        return "/user/info";
+    }
 }
